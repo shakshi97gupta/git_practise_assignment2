d69e7c03aa3124e12f546681afb9fb7ed9e40398
shakshi97gupta <125144159+shakshi97gupta@users.noreply.github.com>
2023-02-21 20:48:49 +0530

    Update prime_problem.js
    i made changes!

--- prime_problem.js
+++ prime_problem.js
@@ -1,8 +1,6 @@
 function identifyPrime(num) {
-<<<<<<< HEAD
-let num=10,
-(num%i==0) ? console.log("Prime") : console.log("Not Prime");
-=======
+
+
 for(let i=2; i<num; i++){
 if(num%i==0){
 console.log("No")
@@ -11,4 +9,4 @@ return;
 }
 console.log("Yes")
 }
->>>>>>> e1937205399cf176358b578566ccac29240d0e0d
+
