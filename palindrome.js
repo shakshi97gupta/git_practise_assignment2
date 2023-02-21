db4f02c2122be697712f53b74deb21b83cce642d
shakshi97gupta <shakshi.97gupta@gmail.com>
2023-02-21 20:58:08 +0530

    added palindrome

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+let name= racecar;
+let bag=""
+for(let i=name.length-1; i>=0; i--){
+bag=bag+name[i];
+}
+if(bag==name){
+console.log("palindrome");
+}
+else{
+console.log("Not Palindrome");
+}
+}
